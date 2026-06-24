11519cd29e2fc6bf46339bff5e042a062688a79e
ian franco barlocco <ian.barlocco73@gmail.com>
2026-06-24 17:13:16 -0300

    agregue reportes (creo q no rompi nada)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -134,6 +134,14 @@
               </svg>
               Mensajes
             </a>
+            <a href="{{ route('ayuda') }}#contacto" class="dropdown-item" role="menuitem">
+              <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
+                <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"/>
+                <line x1="12" y1="9" x2="12" y2="13"/>
+                <line x1="12" y1="17" x2="12.01" y2="17"/>
+              </svg>
+              Reportes
+            </a>
             <a href="{{ route('configuracion') }}" class="dropdown-item" role="menuitem">
               <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M12 3L5 6v6c0 5 3.5 8 7 9 3.5-1 7-4 7-9V6l-7-3z"/>
