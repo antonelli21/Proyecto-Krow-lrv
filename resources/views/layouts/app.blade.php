aab4c61abbbe733c8a912c340337fdb460d0d13f
FABRICIOMOREL04 <fabriciomorel.2004@gmail.com>
2026-06-26 00:08:36 -0300

    Detalles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -134,7 +134,7 @@
               </svg>
               Mensajes
             </a>
-            <a href="{{ route('ayuda') }}#contacto" class="dropdown-item" role="menuitem">
+            <a href="{{ route('ayuda', ['#Contacto']) }}" class="dropdown-item" role="menuitem">
               <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
                 <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"/>
                 <line x1="12" y1="9" x2="12" y2="13"/>
